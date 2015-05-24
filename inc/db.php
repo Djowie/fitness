17fa70de506b1accda37b264043a9c7f1836d059
Djowie <joeybakx@gmail.com>
2015-05-24 19:00:21 +0200

    updated db file for others

--- inc/db.php
+++ inc/db.php
@@ -6,9 +6,9 @@
  */
 function createPDO() {
     // MySQL connection
-    $dbUser = 'root';
-    $dbPass = '';
-    $dbName = 'fitness';
+    $dbUser = 'Your MySQL Username';
+    $dbPass = 'Your MySQL Password';
+    $dbName = 'Your MySQL databasename';
 
     try {
         $db = new PDO("mysql:dbname=$dbName;charset=utf8", $dbUser, $dbPass, array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
